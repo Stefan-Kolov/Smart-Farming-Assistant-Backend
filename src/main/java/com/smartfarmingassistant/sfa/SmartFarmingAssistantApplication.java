25c393f810399c67a754aed152c80fdcb90d4664
Stefan-Kolov <kolov723@gmail.com>
2026-04-05 20:09:19 +0200

    Initial commit

---
+++ src/main/java/com/smartfarmingassistant/sfa/SmartFarmingAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.smartfarmingassistant.sfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFarmingAssistantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartFarmingAssistantApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/smartfarmingassistant/sfa/SmartFarmingAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartfarmingassistant.sfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFarmingAssistantApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
